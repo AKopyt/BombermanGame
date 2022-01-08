a0af8b3145576e5638b3b9c55b5d352ca342ee95
Agata Kopyt <agatakopyt@192.168.0.144>
2022-01-08 17:29:28 +0100

    pygame window

--- RunApplication.py
+++ RunApplication.py
@@ -1,2 +1,30 @@
-print("Hello world")
-print("Hello world")
+import pygame
+import sys
+
+
+
+class Window:
+    def __init__(self):
+        pygame.init()
+        self.screen_resolution = pygame.display.set_mode((500,500))
+        pygame.display.set_caption("Bomberman")
+
+    def run_game(self):
+        while True:
+            self.check_events()
+            self.update_screen()
+
+    def update_screen(self):
+        pygame.display.flip()
+
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+NaszeOkno = Window()
+NaszeOkno.run_game()
+
+
