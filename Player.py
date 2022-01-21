3d6a96215b37812538fd24df85830ec280d69c81
Agata Kopyt <agatakopyt@192.168.0.144>
2022-01-21 08:54:25 +0100

    moving

--- Player.py
+++ Player.py
@@ -1,14 +1,34 @@
 import pygame
 
 class  Player:
-    def __init__(self,screen, screen_width, screen_height):
+    def __init__(self,screen, screen_width, screen_height,settings):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.screen = screen
+        self.settings = settings
 
         self.image = pygame.image.load('images/player.png')
         self.image = pygame.transform.smoothscale(self.image, (self.screen_width*0.1, self.screen_height*0.1))
         self.rect = self.image.get_rect()
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right:
+            self.x += self.settings.player_speed
+        if self.moving_left:
+            self.x -= self.settings.player_speed
+        if self.moving_up:
+            self.y -= self.settings.player_speed
+        if self.moving_down:
+            self.y += self.settings.player_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
--- RunApplication.py
+++ RunApplication.py
@@ -1,6 +1,8 @@
 from Window import Window
 from Player import Player
 from Board import Board
+from Settings import Settings
+import sys
 import pygame
 
 class RunAplication:
@@ -10,7 +12,8 @@ class RunAplication:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.init()
 
-        self.player = Player(self.screen, self.width, self.height)
+        self.settings = Settings()
+        self.player = Player(self.screen, self.width, self.height,self.settings)
         self.board = Board(self.screen, self.width, self.height)
 
         self.lista = []
@@ -18,8 +21,37 @@ class RunAplication:
         self.lista.append(self.player)
 
         self.NaszeOkno = Window(self.lista, self.width, self.height)
-        self.NaszeOkno.run_game()
+
+    def run_game(self):
+        while True:
+            self.check_events()
+            self.player.update()
+            self.NaszeOkno.update_screen()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.player.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.player.moving_left = True
+                if event.key == pygame.K_UP:
+                    self.player.moving_up = True
+                if event.key == pygame.K_DOWN:
+                    self.player.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.player.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.player.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.player.moving_up = False
+                if event.key == pygame.K_DOWN:
+                    self.player.moving_down = False
 
 rn = RunAplication()
+rn.run_game()
 
 
